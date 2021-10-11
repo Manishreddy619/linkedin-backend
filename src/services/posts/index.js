1f8f657ce2bd84960fa8169778b68bc2afbf7ca1
marcosaccarola <marco.sacca.ads@gmail.com>
2021-10-11 13:08:28 +0200

    POST new post

--- src/services/posts/index.js
+++ src/services/posts/index.js
@@ -12,5 +12,14 @@ posts.get('/',async(req,res,next)=>{
         next(error)
     }
 })
+posts.post('/',async(req,res,next)=>{
+    try {
+        const newPost=new postModel(req.body)
+        const{_id}=await newPost.save()
+        res.send(_id)
+    } catch (error) {
+        next(error)
+    }
+})
 
 export default posts
--- src/services/posts/schema.js
+++ src/services/posts/schema.js
@@ -5,7 +5,8 @@ const{Schema,model}=mongoose
 const postSchema=new Schema({
     text:{type:String,required:true},
     username:{type:String,required:true},
-    image:{type:String,required:true}
+    image:{type:String,required:true},
+    user:{tyoe:Object}
 },{timestamps:true})
 
 export default model('post',postSchema)
