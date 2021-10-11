8e76629b41e4f2cb29edec9626cdaa7b9cd0f7a5
marcosaccarola <marco.sacca.ads@gmail.com>
2021-10-11 12:26:45 +0200

    Post Schema

---
+++ src/services/posts/schema.js
@@ -0,0 +1,11 @@
+import mongoose, { model } from 'mongoose'
+
+const{Schema,model}=mongoose
+
+const postSchema=new Schema({
+    text:{type:String,required:true},
+    username:{type:String,required:true},
+    image:{type:String,required:true}
+},{timestamps:true})
+
+export default model('post',postSchema)
