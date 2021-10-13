5e1df04a8ab1d89db0e57c81973b786e6e2b61ff
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-10-13 15:59:26 +0200

    commit

--- src/services/profiles/experienceSchema.js
+++ src/services/profiles/experienceSchema.js
@@ -1,36 +1,36 @@
 ///experience schma..
-import database from "mongoose";
+import database from 'mongoose';
 const { model, Schema } = database;
 const experience = new Schema(
-  {
-    role: {
-      type: String,
-      required: true,
-    },
-    company: {
-      type: String,
-      required: true,
-    },
-    startDate: {
-      type: String,
-      required: true,
-    },
-    endDate: {
-      type: String,
-      required: false,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    area: {
-      type: String,
-      required: true,
-    },
-    username: String,
-    image: { type: String, required: true },
-  },
-  { timestamps: true }
+	{
+		role: {
+			type: String,
+			required: true,
+		},
+		company: {
+			type: String,
+			required: true,
+		},
+		startDate: {
+			type: String,
+			required: true,
+		},
+		endDate: {
+			type: String,
+			required: false,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		area: {
+			type: String,
+			required: true,
+		},
+		username: String,
+		image: { type: String },
+	},
+	{ timestamps: true },
 );
 
-export default model("Experience", experience);
+export default model('Experience', experience);
--- src/services/profiles/index.js
+++ src/services/profiles/index.js
@@ -15,48 +15,40 @@ const profileRouter = express.Router();
 
 //const experienceRoutes = express.Router();
 
-profileRouter.post(
-	'/:username/experiences',
-	parseFile.single('image'),
-	fileIsRequired,
-	async (req, res, next) => {
-		try {
-			const users = await profileModel.find();
-			if (users) {
-				const getPname = users.find(
-					(usr) => usr.username === req.params.username,
-				);
-				console.log(getPname);
-				if (getPname) {
-					const myObj = {
-						...req.body,
-						username: req.params.username,
-						image: req.file.path,
-					};
+profileRouter.post('/:username/experiences', async (req, res, next) => {
+	try {
+		const users = await profileModel.find();
+		if (users) {
+			const getPname = users.find(
+				(usr) => usr.username === req.params.username,
+			);
+			console.log(getPname);
+			if (getPname) {
+				const myObj = {
+					...req.body,
+					username: req.params.username,
+				};
 
-					const newExperience = new experienceModel(myObj);
-					const exp = await newExperience.save();
-					if (exp) {
-						const userDetails = await profileModel.findByIdAndUpdate(
-							getPname._id,
-							{ $push: { experiences: exp } },
-							{ new: true },
-						);
-						res.status(201).send(newExperience);
-					}
-				} else {
-					res
-						.status(401)
-						.send(
-							`the profile name with ${req.params.username} was not found.`,
-						);
+				const newExperience = new experienceModel(myObj);
+				const exp = await newExperience.save();
+				if (exp) {
+					const userDetails = await profileModel.findByIdAndUpdate(
+						getPname._id,
+						{ $push: { experiences: exp } },
+						{ new: true },
+					);
+					res.status(201).send(newExperience);
 				}
+			} else {
+				res
+					.status(401)
+					.send(`the profile name with ${req.params.username} was not found.`);
 			}
-		} catch (error) {
-			console.log(error);
 		}
-	},
-);
+	} catch (error) {
+		console.log(error);
+	}
+});
 profileRouter.get('/:username/experiences', async (req, res, next) => {
 	try {
 		const users = await profileModel.find();
@@ -107,7 +99,6 @@ profileRouter.get('/:username/experiences/:exId', async (req, res, next) => {
 });
 profileRouter.put(
 	'/:username/experiences/:exId',
-	parseFile.single('image'),
 	fileIsRequired,
 	async (req, res, next) => {
 		try {
@@ -121,7 +112,6 @@ profileRouter.put(
 					const myObj = {
 						...req.body,
 						username: req.params.username,
-						image: req.file.path,
 					};
 
 					const updateExp = await experienceModel.findByIdAndUpdate(
@@ -239,49 +229,42 @@ profileRouter.get(
 
 //export default experienceRoutes;
 //------------------------------------------------------create users-------------------------------------
-profileRouter.post(
-	'/',
-	parseFile.single('image'),
-	fileIsRequired,
-	profileValidator,
-	async (req, res, next) => {
-		try {
-			const errors = validationResult(req);
-			if (!errors.isEmpty()) {
-				next(createHttpError(400, { message: errors }));
+profileRouter.post('/', profileValidator, async (req, res, next) => {
+	try {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			next(createHttpError(400, { message: errors }));
+		} else {
+			const { name, surname, email, bio, title, area, username } = req.body;
+			const users = await profileModel.find();
+			let userNameExists = users.find((user) => user.username === username);
+			console.log(userNameExists?.username);
+			if (userNameExists) {
+				next(
+					createHttpError(400, {
+						message: 'username already exists try with another',
+					}),
+				);
 			} else {
-				const { name, surname, email, bio, title, area, username } = req.body;
-				const users = await profileModel.find();
-				let userNameExists = users.find((user) => user.username === username);
-				console.log(userNameExists?.username);
-				if (userNameExists) {
-					next(
-						createHttpError(400, {
-							message: 'username already exists try with another',
-						}),
-					);
-				} else {
-					const newUser = {
-						name,
-						surname,
-						email,
-						bio,
-						title,
-						area,
-						username,
-						image: req.file.path,
-					};
-					console.log(newUser);
-					const newProfile = new profileModel(newUser);
-					const { _id } = await newProfile.save();
-					res.status(201).send(_id);
-				}
+				const newUser = {
+					name,
+					surname,
+					email,
+					bio,
+					title,
+					area,
+					username,
+				};
+				console.log(newUser);
+				const newProfile = new profileModel(newUser);
+				const { _id } = await newProfile.save();
+				res.status(201).send(_id);
 			}
-		} catch (error) {
-			next(error);
 		}
-	},
-);
+	} catch (error) {
+		next(error);
+	}
+});
 profileRouter.get('/', async (req, res, next) => {
 	const users = await profileModel.find();
 
@@ -303,35 +286,52 @@ profileRouter.get('/:userId', async (req, res, next) => {
 		next(error);
 	}
 });
-profileRouter.put(
-	'/:userId',
-	parseFile.single('image'),
-	fileIsRequired,
-	profileValidator,
-	async (req, res, next) => {
-		try {
-			const id = req.params.userId;
-			const user = await profileModel.findById(id);
-			if (user) {
-				const errors = validationResult(req);
-				if (!errors.isEmpty()) {
-					next(createHttpError(400, { message: errors }));
-				} else {
-					const { name, surname, email, bio, title, area, username } = req.body;
-					const users = await profileModel.find();
-					const user = await profileModel.findById(id);
-					const newUser = {
-						...user.toObject(),
-						name,
-						surname,
-						email,
-						bio,
-						title,
-						area,
-						username,
-						image: req.file?.path,
-					};
-					if (user.username === username) {
+profileRouter.put('/:userId', profileValidator, async (req, res, next) => {
+	try {
+		const id = req.params.userId;
+		const user = await profileModel.findById(id);
+		if (user) {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				next(createHttpError(400, { message: errors }));
+			} else {
+				const { name, surname, email, bio, title, area, username } = req.body;
+				const users = await profileModel.find();
+				const user = await profileModel.findById(id);
+				const newUser = {
+					...user.toObject(),
+					name,
+					surname,
+					email,
+					bio,
+					title,
+					area,
+					username,
+				};
+				if (user.username === username) {
+					const modifiedProfile = await profileModel.findByIdAndUpdate(
+						id,
+						newUser,
+						{
+							new: true,
+						},
+					);
+					if (modifiedProfile) {
+						res.send(modifiedProfile);
+					} else {
+						next(createHttpError(404, `user with id ${id} not found!`));
+					}
+				}
+				if (user.username !== username) {
+					let userNameExists = users.find((user) => user.username === username);
+
+					if (userNameExists) {
+						next(
+							createHttpError(400, {
+								message: 'username already exists try with another',
+							}),
+						);
+					} else {
 						const modifiedProfile = await profileModel.findByIdAndUpdate(
 							id,
 							newUser,
@@ -345,41 +345,15 @@ profileRouter.put(
 							next(createHttpError(404, `user with id ${id} not found!`));
 						}
 					}
-					if (user.username !== username) {
-						let userNameExists = users.find(
-							(user) => user.username === username,
-						);
-
-						if (userNameExists) {
-							next(
-								createHttpError(400, {
-									message: 'username already exists try with another',
-								}),
-							);
-						} else {
-							const modifiedProfile = await profileModel.findByIdAndUpdate(
-								id,
-								newUser,
-								{
-									new: true,
-								},
-							);
-							if (modifiedProfile) {
-								res.send(modifiedProfile);
-							} else {
-								next(createHttpError(404, `user with id ${id} not found!`));
-							}
-						}
-					}
 				}
-			} else {
-				next(createHttpError(404, `user with id ${id} not found!`));
 			}
-		} catch (error) {
-			next(error);
+		} else {
+			next(createHttpError(404, `user with id ${id} not found!`));
 		}
-	},
-);
+	} catch (error) {
+		next(error);
+	}
+});
 profileRouter.delete('/:userId', async (req, res, next) => {
 	try {
 		const user = await profileModel.findByIdAndDelete(req.params.userId);
