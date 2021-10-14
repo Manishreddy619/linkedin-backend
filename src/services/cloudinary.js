e9093e24b0f5be54225bc750db0d9c015de2d5f5
kolluvinay425 <kolluvinay425@gmail.com>
2021-10-14 20:03:55 +0200

    update

--- src/services/cloudinary.js
+++ src/services/cloudinary.js
@@ -1,13 +1,13 @@
-import { v2 as cloudinary } from 'cloudinary';
-import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { v2 as cloudinary } from "cloudinary";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 const { CLOUDINARY_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET } = process.env;
 
 cloudinary.config({
-	cloud_name: CLOUDINARY_NAME,
-	api_key: CLOUDINARY_KEY,
-	api_secret: CLOUDINARY_SECRET,
+  cloud_name: CLOUDINARY_NAME,
+  api_key: CLOUDINARY_KEY,
+  api_secret: CLOUDINARY_SECRET,
 });
 // CLOUDINARY_NAME = dytffimtv;
 
@@ -16,6 +16,6 @@ cloudinary.config({
 // CLOUDINARY_SECRET = Hewe0wS4 - N836NHy3tUOEtgkyYk;
 
 const storage = new CloudinaryStorage({
-	cloudinary: cloudinary,
+  cloudinary: cloudinary,
 });
 export const parseFile = multer({ storage });
